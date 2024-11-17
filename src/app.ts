d4cd482992c115639defe641fa1d2d378c670e60
Moshfiqur Rahman <studentemam@gmail.com>
2024-11-17 10:29:17 +0600

    config: add prettier configurations and ignore files

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,9 @@
-import express, { Request, Response } from "express";
+import express, { Request, Response } from 'express';
+
 const app = express();
 
-app.get("/health", (req: Request, res: Response) => {
-    res.send("Server is running...");
-})
+app.get('/health', (req: Request, res: Response) => {
+  res.send('Server is running...');
+});
 
 export default app;
