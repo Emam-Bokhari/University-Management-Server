98a2c986f34381131bfc2ba88b653b0976bc8a5f
Moshfiqur Rahman <studentemam@gmail.com>
2024-11-18 09:56:12 +0600

    feat(api): implement endpoint to retrieve all students - added GET route (GET /api/v1/students) to fetch all student records - implemented StudentControllers.getAllStudents to handle the request - integrated StudentServices.getAllStudentsFromDB to fetch data from the database - ensured response includes success message and list of students

--- src/app/modules/student/student.route.ts
+++ src/app/modules/student/student.route.ts
@@ -5,4 +5,7 @@ const router = express.Router();
 
 router.post('/', StudentControllers.createStudent);
 
+router.get('/', StudentControllers.getAllStudents);
+
+
 export const StudentRoutes = router;
