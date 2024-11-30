84cc18cb4c4fb402dee8c9db7642a117294aa2b5
Moshfiqur Rahman <studentemam@gmail.com>
2024-11-30 23:23:35 +0600

    feat(academicSemester): add academicSemester interface

---
+++ src/app/modules/academicSemester/academicSemester.interface.ts
@@ -0,0 +1,9 @@
+export type TMonths = "January" | "February" | "March" | "April" | "May" | "June" | "July" | "August" | "September" | "October" | "November" | "December";
+
+export type TAcademicSemester = {
+    name: "Autumn" | "Summer" | "Fall";
+    year: Date;
+    code: "01" | "02" | "03";
+    startMonth: TMonths;
+    endMonth: TMonths;
+}
