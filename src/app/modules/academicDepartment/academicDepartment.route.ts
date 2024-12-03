d17cb1173d3a26bdcd48ac6eaabba4fd2cd35447
Moshfiqur Rahman <studentemam@gmail.com>
2024-12-03 22:54:16 +0600

    feat(academicDepartment): add validation for creation and updating academic department

--- src/app/modules/academicDepartment/academicDepartment.route.ts
+++ src/app/modules/academicDepartment/academicDepartment.route.ts
@@ -1,14 +1,16 @@
 import express from "express";
 import { AcademicDepartmentControllers } from "./academicDepartment.controller";
+import { validateRequest } from "../../middlewares/validateRequest";
+import { academicDepartmentValidationSchema } from "./academicDepartment.validation";
 
 const router = express.Router();
 
-router.post("/create-academic-department", AcademicDepartmentControllers.createAcademicDepartment)
+router.post("/create-academic-department", validateRequest(academicDepartmentValidationSchema.createAcademicDepartmentValidationSchema), AcademicDepartmentControllers.createAcademicDepartment)
 
 router.get("/", AcademicDepartmentControllers.getAllAcademicDepartment)
 
 router.get("/:academicDepartmentId", AcademicDepartmentControllers.getSingleAcademicDepartment);
 
-router.patch("/:academicDepartmentId", AcademicDepartmentControllers.updateAcademicDepartment);
+router.patch("/:academicDepartmentId", validateRequest(academicDepartmentValidationSchema.updateAcademicDepartmentValidationSchema), AcademicDepartmentControllers.updateAcademicDepartment);
 
 export const AcademicDepartmentRoutes = router;
