803780b1dbb996879987e52999a1caef1b3c219b
Moshfiqur Rahman <studentemam@gmail.com>
2024-12-04 19:21:17 +0600

    feat: add transction handling for student creation

--- src/app/modules/user/user.service.ts
+++ src/app/modules/user/user.service.ts
@@ -1,3 +1,4 @@
+import mongoose from 'mongoose';
 import config from '../../config';
 import AppError from '../../errors/AppError';
 import { AcademicSemester } from '../academicSemester/academicSemester.model';
@@ -26,22 +27,64 @@ const createStudentIntoDB = async (password: string, payload: TStudent) => {
     throw new AppError(404, 'Academic semester not found!');
   }
 
-  // set manually generated if
-  userData.id = await generateStudentId(admissionSemester);
+  const session = await mongoose.startSession();
 
-  // create a user
-  const newUser = await User.create(userData);
+  try {
+    session.startTransaction()
+    // set manually generated if
+    userData.id = await generateStudentId(admissionSemester);
 
-  // create a student
-  if (Object.keys(newUser).length) {
-    payload.id = newUser.id; // embedding ID
-    payload.user = newUser._id; // reference ID
+    // transction:1
+    // create a user
+    const newUser = await User.create([userData], { session });
 
-    const newStudent = await Student.create(payload);
+    // create a student
+    if (!newUser.length) {
+      throw new AppError(400, "Failed to create user!")
+    }
+
+    payload.id = newUser[0].id; // embedding ID
+    payload.user = newUser[0]._id; // reference ID
+
+    // transction:2
+    const newStudent = await Student.create([payload], { session });
+
+    if (!newStudent.length) {
+      throw new AppError(400, "Failed to create student!")
+    }
+    await session.commitTransaction()
+    await session.endSession()
+
     return newStudent;
+
+  } catch (err) {
+    await session.abortTransaction()
+    await session.endSession()
+    throw err;
   }
 };
 
 export const UserServices = {
   createStudentIntoDB,
 };
+
+/* 
+transction-->Steps
+Step:1
+start session
+Step:2
+start transction
+Step:3
+data k array hisebe pathano, abong sathe session ta pathiye daoua
+Step:4
+error ta throw kore daoua
+Step:5
+successfull hoile commit transction kore daoua
+Step:6
+session ta end kore daoua
+Step:7
+abort transction kora
+Step:8
+session ta end kora
+
+*/
