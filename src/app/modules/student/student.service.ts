15d090aa7a082181e78108b5d3171b94e0f4eda1
Moshfiqur Rahman <studentemam@gmail.com>
2024-12-04 11:45:30 +0600

    feat(student): populate, and aggregate query admissionSemester,academicDepartment,academicFaculty

--- src/app/modules/student/student.service.ts
+++ src/app/modules/student/student.service.ts
@@ -1,16 +1,50 @@
 import { Student } from './student.model';
 
 const getAllStudentsFromDB = async () => {
-  const result = await Student.find();
+  const result = await Student.find().populate("admissionSemester").populate({
+    path: "academicDepartment",
+    populate: ({
+      path: "academicFaculty"
+    })
+  });
   return result;
 };
 
 const getSingleStudentFromDB = async (id: string) => {
   // const result = await Student.findOne({ id: id });
-  const result = Student.aggregate([
+  const result = await Student.aggregate([
     //stage:1
     { $match: { id: id } },
-  ]);
+    //stage:2
+    {
+      $lookup: {
+        from: "academicsemesters", // collection name
+        localField: "admissionSemester", // field in the student model
+        foreignField: "_id", // field in the admission semester
+        as: "admissionSemester" // alias name of populated data
+      }
+    },
+    //stage:3
+    {
+      $lookup: {
+        from: "academicdepartments",
+        localField: "academicDepartment",
+        foreignField: "_id",
+        as: "academicDepartment"
+      }
+    },
+    //stage:4
+    { $unwind: "$academicDepartment" },
+    //stage:5
+    {
+      $lookup: {
+        from: "academicfaculties",
+        localField: "academicDepartment.academicFaculty",
+        foreignField: "_id",
+        as: "academicDepartment.academicFaculty"
+      }
+    }
+  ])
   return result;
 };
 
