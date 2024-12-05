5aecae8d4c9c79edb14ab24e6cdb7ccf980b6cad
Moshfiqur Rahman <studentemam@gmail.com>
2024-12-05 10:47:08 +0600

    feat(student): validate before creation and updating student

--- src/app/modules/student/student.route.ts
+++ src/app/modules/student/student.route.ts
@@ -1,13 +1,15 @@
 import express from 'express';
 import { StudentControllers } from './student.controller';
+import { validateRequest } from '../../middlewares/validateRequest';
+import { studentValidationSchema } from './student.validation';
 
 const router = express.Router();
 
 router.get('/', StudentControllers.getAllStudents);
 
-router.get('/:studentId', StudentControllers.getSingleStudent);
+router.get('/:studentId', validateRequest(studentValidationSchema.createStudentValidationSchema), StudentControllers.getSingleStudent);
 
-router.patch("/:studentId", StudentControllers.updateStudent);
+router.patch("/:studentId", validateRequest(studentValidationSchema.updateStudentValidationSchema), StudentControllers.updateStudent);
 
 router.delete('/:studentId', StudentControllers.deleteStudent);
 
