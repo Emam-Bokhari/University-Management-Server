2b730f61e1edc322f4ceb932319b3486fd837a96
Moshfiqur Rahman <studentemam@gmail.com>
2024-12-06 10:25:25 +0600

    feat: add handleDuplicateError function to manage duplicate key errors

---
+++ src/app/errors/handleDuplicateError.ts
@@ -0,0 +1,23 @@
+import { TErrorSource, TGenericErrorResponse } from "../interface/error";
+
+export const handleDuplicateError = (err: any): TGenericErrorResponse => {
+
+    const match = err.message.match(/"([^"]*)"/);
+
+    const extracedMessage = match && match[1];
+
+    const errorSources: TErrorSource = [
+        {
+            path: "",
+            message: `${extracedMessage} is already exist!`,
+        }
+    ]
+
+    const statusCode = 400;
+
+    return {
+        statusCode,
+        message: "Duplicate Key Error: Conflict with an existing entry.",
+        errorSources,
+    }
+}
