04aa9b4571b7ea2af003e1a5e82ecd5d627f8790
Moshfiqur Rahman <studentemam@gmail.com>
2024-12-08 11:31:12 +0600

    feat(admin): implement to handle nonprimitve nested data update in admin

--- src/app/modules/admin/admin.Service.ts
+++ src/app/modules/admin/admin.Service.ts
@@ -5,63 +5,77 @@ import AppError from '../../errors/AppError';
 import { User } from '../user/user.model';
 
 const getAllAdminFromDB = async () => {
-  const result = await Admin.find();
+    const result = await Admin.find();
 
-  return result;
+    return result;
 };
 
 const getSingleAdminFromDB = async (adminId: string) => {
-  const result = await Admin.findOne({ id: adminId });
+    const result = await Admin.findOne({ id: adminId });
 
-  return result;
+    return result;
 };
 
 const updateAdminIntoDB = async (adminId: string, payload: Partial<TAdmin>) => {
-  const result = await Admin.updateOne({ id: adminId }, payload, { new: true });
 
-  return result;
+    const { name, ...remainingAdminData } = payload;
+
+    const modefiedUpdatedData: Record<string, unknown> = {
+        ...remainingAdminData
+    }
+
+    if (name && Object.keys(name).length) {
+        for (const [key, value] of Object.entries(name)) {
+            modefiedUpdatedData[`name.${key}`] = value;
+        }
+    }
+
+
+    const result = await Admin.updateOne({ id: adminId }, modefiedUpdatedData, { new: true });
+
+    return result;
 };
 
 const deleteAdminFromDB = async (adminId: string) => {
-  const session = await mongoose.startSession();
+    const session = await mongoose.startSession();
 
-  try {
-    session.startTransaction();
+    try {
+        session.startTransaction();
 
-    const deletedAdmin = await Admin.updateOne(
-      { id: adminId },
-      { isDeleted: true },
-      { new: true, session },
-    );
+        const deletedAdmin = await Admin.updateOne(
+            { id: adminId },
+            { isDeleted: true },
+            { new: true, session },
+        );
 
-    if (!deletedAdmin) {
-      throw new AppError(400, 'Failed to delete admin!');
+        if (!deletedAdmin) {
+            throw new AppError(400, 'Failed to delete admin!');
+        }
+
+        const deleteUser = await User.updateOne(
+            { id: adminId },
+            { isDeleted: true },
+            { new: true, session },
+        );
+
+        if (!deleteUser) {
+            throw new AppError(400, 'Failed to delete user!');
+        }
+
+        await session.commitTransaction();
+        await session.endSession();
+
+        return deletedAdmin;
+    } catch (err) {
+        await session.abortTransaction();
+        await session.endSession();
+        throw err;
     }
-
-    const deleteUser = await User.updateOne(
-      { id: adminId },
-      { isDeleted: true },
-      { new: true, session },
-    );
-
-    if (!deleteUser) {
-      throw new AppError(400, 'Failed to delete user!');
-    }
-
-    await session.commitTransaction();
-    await session.endSession();
-
-    return deletedAdmin;
-  } catch (err) {
-    await session.abortTransaction();
-    await session.endSession();
-    throw err;
-  }
 };
 
 export const AdminServices = {
-  getAllAdminFromDB,
-  getSingleAdminFromDB,
-  updateAdminIntoDB,
-  deleteAdminFromDB,
+    getAllAdminFromDB,
+    getSingleAdminFromDB,
+    updateAdminIntoDB,
+    deleteAdminFromDB,
 };
