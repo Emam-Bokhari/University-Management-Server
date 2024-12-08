bd7ef75ec02973e785d93426281095aa106acec2
Moshfiqur Rahman <studentemam@gmail.com>
2024-12-08 10:25:57 +0600

    feat(admin): implement handler for retrieving all admins

---
+++ src/app/modules/admin/admin.controller.ts
@@ -0,0 +1,19 @@
+import { RequestHandler } from "express";
+import { catchAsync } from "../../utils/catchAsync";
+import { AdminServices } from "./admin.Service";
+import sendResponse from "../../utils/sendResponse";
+
+const getAllAdmin: RequestHandler = catchAsync(async (req, res) => {
+    const result = await AdminServices.getAllAdminFromDB();
+
+    sendResponse(res, {
+        statusCode: 200,
+        success: true,
+        message: "Admin are retrieved successfully",
+        data: result,
+    })
+})
+
+export const AdminControllers = {
+    getAllAdmin,
+}
