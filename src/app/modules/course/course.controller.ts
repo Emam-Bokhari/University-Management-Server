67e78be3d83a77883345dcc705ce2ee47ea60fe7
Moshfiqur Rahman <studentemam@gmail.com>
2024-12-08 20:32:30 +0600

    feat(course): implement CRUD controllers for course

---
+++ src/app/modules/course/course.controller.ts
@@ -0,0 +1,58 @@
+import { RequestHandler } from "express";
+import { catchAsync } from "../../utils/catchAsync";
+import { CourseServices } from "./course.service";
+import sendResponse from "../../utils/sendResponse";
+
+const createCourse: RequestHandler = catchAsync(async (req, res) => {
+    const courseData = req.body;
+    const result = await CourseServices.createCourseIntoDB(courseData);
+
+    sendResponse(res, {
+        statusCode: 201,
+        success: true,
+        message: "Created course successfully",
+        data: result,
+    })
+})
+
+const getAllCourses: RequestHandler = catchAsync(async (req, res) => {
+    const result = await CourseServices.getAllCoursesFromDB();
+
+    sendResponse(res, {
+        statusCode: 200,
+        success: true,
+        message: "Course are retrieved successfully",
+        data: result,
+    })
+})
+
+const getSingleCourse: RequestHandler = catchAsync(async (req, res) => {
+    const courseId = req.params.courseId;
+    const result = await CourseServices.getSingleCourseFromDB(courseId);
+
+    sendResponse(res, {
+        statusCode: 200,
+        success: true,
+        message: "Course is retrieved successfully",
+        data: result,
+    })
+})
+
+const deleteCourse: RequestHandler = catchAsync(async (req, res) => {
+    const courseId = req.params.courseId;
+    const result = await CourseServices.deleteCourseFromDB(courseId);
+
+    sendResponse(res, {
+        statusCode: 200,
+        success: true,
+        message: "Course is deleted successfully",
+        data: result,
+    })
+})
+
+export const CourseControllers = {
+    createCourse,
+    getAllCourses,
+    getSingleCourse,
+    deleteCourse,
+}
