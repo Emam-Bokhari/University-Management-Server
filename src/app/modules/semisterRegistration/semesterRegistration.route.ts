1b674805a41af260b6b1a0a535d6cdb90ca92658
Moshfiqur Rahman <studentemam@gmail.com>
2024-12-10 20:25:28 +0600

    feat(semesterRegistration): add validaton middleware before creating semester registration

--- src/app/modules/semisterRegistration/semesterRegistration.route.ts
+++ src/app/modules/semisterRegistration/semesterRegistration.route.ts
@@ -1,8 +1,10 @@
 import express from "express";
 import { SemesterRegistrationControllers } from "./semesterRegistration.controller";
+import { validateRequest } from "../../middlewares/validateRequest";
+import { semesterRegistrationValidationSchema } from "./semesterRegistration.validation";
 
 const router = express.Router()
 
-router.post("/create-semester-registration", SemesterRegistrationControllers.createSemesterRegistration)
+router.post("/create-semester-registration", validateRequest(semesterRegistrationValidationSchema.createSemesterRegistrationValidationSchema), SemesterRegistrationControllers.createSemesterRegistration)
 
 export const SemesterRegistrationRoutes = router;
