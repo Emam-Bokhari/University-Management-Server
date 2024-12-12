040aa9cfad27a02f64750e104bde015b1aa7580c
Moshfiqur Rahman <studentemam@gmail.com>
2024-12-12 19:53:39 +0600

    feat(offeredCourse): fix bug in time validation

--- src/app/modules/offeredCourse/offeredCourse.validation.ts
+++ src/app/modules/offeredCourse/offeredCourse.validation.ts
@@ -19,15 +19,18 @@ const createOfferedCourseValidationSchema = z.object({
             const regex = /^([01]?[0-9]|2[0-3]):[0-5][0-9]$/;
             return regex.test(time);
         }),
-    }).refine((body) => {
-        const start = new Date(`1970-01-01T${body.startTime}.00`);
-        const end = new Date(`1970-01-01T${body.endTime}.00`);
+    }).refine(
+        (body) => {
+            const start = new Date(`1970-01-01T${body.startTime}:00`);
+            const end = new Date(`1970-01-01T${body.endTime}:00`);
 
-        return end > start;
-    }, {
-        message: "Start time should be before End time"
-    })
-})
+            return end > start;
+        },
+        {
+            message: 'Start time should be before End time !  ',
+        },
+    ),
+});
 
 const updateOfferedCourseValidationSchema = z.object({
     body: z.object({
