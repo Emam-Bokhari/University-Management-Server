e342cd1dfa88079e00f635c73f9afa74aea6724d
Moshfiqur Rahman <studentemam@gmail.com>
2024-12-12 11:35:51 +0600

    feat(offeredCourse): add route for creating offeredCourse

---
+++ src/app/modules/offeredCourse/offeredCourse.route.ts
@@ -0,0 +1,8 @@
+import express from "express";
+import { OfferedCourseControllers } from "./offeredCourse.controller";
+
+const router = express.Router();
+
+router.post("/create-offered-course", OfferedCourseControllers.createOfferedCourse)
+
+export const OfferedCourseRoutes = router;
