95da17861ad9ed93cfd59c6dcedc39a8a0e2b417
Moshfiqur Rahman <studentemam@gmail.com>
2024-12-16 11:31:12 +0600

    feat(auth): add validation schema for change password

--- src/app/modules/auth/auth.route.ts
+++ src/app/modules/auth/auth.route.ts
@@ -11,4 +11,6 @@ router.post(
   AuthControllers.loginUser,
 );
 
+router.post("/change-password")
+
 export const AuthRoutes = router;
--- src/app/modules/user/user.model.ts
+++ src/app/modules/user/user.model.ts
@@ -12,6 +12,7 @@ const userSchema = new Schema<TUser, UserModel>(
     },
     password: {
       type: String,
+      select: 0,
     },
     needsPasswordChange: {
       type: Boolean,
