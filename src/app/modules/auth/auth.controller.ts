ead383b3afc798c9c48286c635bd625b70161883
Moshfiqur Rahman <studentemam@gmail.com>
2024-12-18 20:24:39 +0600

    feat:  Set refresh token as HttpOnly cookie with secure flag

--- src/app/modules/auth/auth.controller.ts
+++ src/app/modules/auth/auth.controller.ts
@@ -2,15 +2,26 @@ import { RequestHandler } from 'express';
 import { catchAsync } from '../../utils/catchAsync';
 import { AuthServices } from './auth.service';
 import sendResponse from '../../utils/sendResponse';
+import config from '../../config';
 
 const loginUser: RequestHandler = catchAsync(async (req, res) => {
   const result = await AuthServices.loginUser(req.body);
+
+  const { refreshToken, accessToken, needsPasswordChange } = result;
+
+  res.cookie("refreshToken", refreshToken, {
+    secure: config.node_env === "production",
+    httpOnly: true,
+  })
 
   sendResponse(res, {
     statusCode: 201,
     success: true,
     message: 'This is a authenticate user',
-    data: result,
+    data: {
+      accessToken,
+      needsPasswordChange,
+    },
   });
 });
 
