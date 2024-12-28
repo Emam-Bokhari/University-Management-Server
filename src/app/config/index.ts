096f84561b2e0f562031a30d2a81071597610755
Moshfiqur Rahman <studentemam@gmail.com>
2024-12-28 18:51:50 +0600

    env: add suppor for SUPER_ADMIN_PASSWORD

--- src/app/config/index.ts
+++ src/app/config/index.ts
@@ -19,4 +19,5 @@ export default {
   cloudinary_api_secret: process.env.CLOUDINARY_API_SECRET,
   cloudinary_api_key: process.env.CLOUDINARY_API_KEY,
   cloudinary_cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  super_admin_password: process.env.SUPER_ADMIN_PASSWORD,
 };
