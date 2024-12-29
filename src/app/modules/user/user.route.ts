f04ee8b62f99c147fedf974729a9f65b4b141b6e
Moshfiqur Rahman <studentemam@gmail.com>
2024-12-29 11:53:38 +0600

    feat(user): add role-based authorization for superAdmin and admin

--- src/app/modules/user/user.route.ts
+++ src/app/modules/user/user.route.ts
@@ -13,7 +13,7 @@ const router = express.Router();
 
 router.post(
   '/create-student',
-  // auth('admin'),
+  auth(USER_ROLE.superAdmin, USER_ROLE.admin),
   upload.single('file'),
   (req: Request, res: Response, next: NextFunction) => {
     req.body = JSON.parse(req.body.data);
